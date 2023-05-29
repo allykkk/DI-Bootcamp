3bb0f8d051e39dceaf83246461755bc2541547c2
allykkk <ally.appskk@gmail.com>
2023-05-29 20:07:53 +0300

    Added Week5 Day2 DailyChallenge

---
+++ Week5/Day2/DailyChallenge/phonebook/phonebook_app/views.py
@@ -0,0 +1,22 @@
+from django.http import HttpResponse
+from django.shortcuts import render,get_object_or_404
+from .models import Person
+
+
+def search_form(request):
+    return render(request, 'search_form.html')
+
+
+def find_by_phone(request):
+    if 'phone_number' in request.GET:
+        phone_number = request.GET['phone_number']
+        person = get_object_or_404(Person, phone_number=phone_number)
+        return render(request, 'person_details.html', {'person': person})
+    return render(request, 'search_form.html')
+
+def find_by_name(request):
+    if 'name' in request.GET:
+        name = request.GET['name']
+        person = get_object_or_404(Person, name=name)
+        return render(request, 'person_details.html', {'person': person})
+    return render(request, 'search_form.html')
