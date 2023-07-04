9ae65f01b1d6ac53fa84e5942a649929f2d225d8
Ally <131651127+allykkk@users.noreply.github.com>
2023-07-04 07:05:33 +0000

    Finished Week9 Day4 Daily Challenge frontend

--- Week9/Day4/CurrencyConverter/script.js
+++ Week9/Day4/CurrencyConverter/script.js
@@ -1,26 +1,26 @@
-const apiKey=''
+const apiKey = ''     // please input your own api key 
 
-async function getCurrencies(){
-    const url=`https://v6.exchangerate-api.com/v6/${apiKey}/codes`
+async function getCurrencies() {
+    const url = `https://v6.exchangerate-api.com/v6/${apiKey}/codes`
     const response = await fetch(url);
-    const data=await response.json()
-    const orginalArray =data.supported_codes
-    let object={} 
+    const data = await response.json()
+    const orginalArray = data.supported_codes
+    let object = {}
     orginalArray.forEach(element => {
-        object[`${element[0]} - ${element[1]}`] =`${element[0]}`      
+        object[`${element[0]} - ${element[1]}`] = `${element[0]}`
     });
     return object // each element looks like : USD-United States Dollar: "USD"
 }
 
 
 
-async function fillDropDown(id){
-    const select=document.getElementById(id)
-    const currencyChoices=await getCurrencies();
-    Object.keys(currencyChoices).forEach((element)=>{
-        const option=document.createElement('option');
-        option.textContent=element;
-        option.value=currencyChoices[element];
+async function fillDropDown(id) {
+    const select = document.getElementById(id)
+    const currencyChoices = await getCurrencies();
+    Object.keys(currencyChoices).forEach((element) => {
+        const option = document.createElement('option');
+        option.textContent = element;
+        option.value = currencyChoices[element];
         select.appendChild(option);
     })
 }
@@ -29,34 +29,34 @@ fillDropDown('currencyOne')
 fillDropDown('currencyTwo')
 
 
-async function getConvertRate(currencyOne,currencyTwo){
-    const url=`https://v6.exchangerate-api.com/v6/${apiKey}/pair/${currencyOne}/${currencyTwo}`
+async function getConvertRate(currencyOne, currencyTwo) {
+    const url = `https://v6.exchangerate-api.com/v6/${apiKey}/pair/${currencyOne}/${currencyTwo}`
     const response = await fetch(url);
-    const data=await response.json()
+    const data = await response.json()
     return data.conversion_rate
 }
 
-console.log(getConvertRate('EUR','GBP'))
+console.log(getConvertRate('EUR', 'GBP'))
 
 
-async function convertCurrencies(){
-    const currencyOne=document.getElementById('currencyOne').value;
-    const currencyTwo=document.getElementById('currencyTwo').value;
-    const convertAmount=document.getElementById('amount').value;
+async function convertCurrencies() {
+    const currencyOne = document.getElementById('currencyOne').value;
+    const currencyTwo = document.getElementById('currencyTwo').value;
+    const convertAmount = document.getElementById('amount').value;
 
-    const convertRate= await getConvertRate(currencyOne,currencyTwo);
+    const convertRate = await getConvertRate(currencyOne, currencyTwo);
 
-    const convertResult=`${convertAmount*convertRate} ${currencyTwo}`;
+    const convertResult = `${convertAmount * convertRate} ${currencyTwo}`;
 
-    const result=document.createElement('p');
-    result.textContent=convertResult;
+    const result = document.createElement('p');
+    result.textContent = convertResult;
     resultContainer.appendChild(result);
-    console.log(currencyOne,currencyTwo,convertAmount,convertRate,convertResult)
+    console.log(currencyOne, currencyTwo, convertAmount, convertRate, convertResult)
 }
 
 
 
-const resultContainer=document.getElementById('result-container');
-const convertBtn=document.getElementById('convert-btn');
+const resultContainer = document.getElementById('result-container');
+const convertBtn = document.getElementById('convert-btn');
 
-convertBtn.addEventListener('click',convertCurrencies)
+convertBtn.addEventListener('click', convertCurrencies)
