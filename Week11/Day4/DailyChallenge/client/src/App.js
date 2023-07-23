9ab059f931cdb8416e161658f1b72ba1afa6921d
allykkk <ally.appskk@gmail.com>
2023-07-23 18:29:21 +0300

    Finished Week11 Day4 DailyChallenge

--- Week11/Day4/DailyChallenge/client/src/App.js
+++ Week11/Day4/DailyChallenge/client/src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className='container'>
       <Header />
-      <PostForm/>
+      <PostForm />
     </div>
   );
 }
--- Week11/Day4/DailyChallenge/client/src/components/postForm.jsx
+++ Week11/Day4/DailyChallenge/client/src/components/postForm.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 
 class PostForm extends Component {
+  state = { message: "" };
+  
   submitHandler = async (event) => {
     event.preventDefault();
     const userInput = event.target[0].value;
@@ -12,7 +14,8 @@ class PostForm extends Component {
     const url = "http://localhost:5000/api/world";
     try {
       const response = await fetch(url, request);
-      console.log(await response.text());
+      const message = await response.text();
+      this.setState({ message });
     } catch (error) {
       console.error("Error:", error);
     }
@@ -24,6 +27,7 @@ class PostForm extends Component {
         <h2>Post to Server:</h2>
         <input name="postMessage" type="text" />
         <button type="submit">Submit</button>
+        <p>{this.state.message}</p>
       </form>
     );
   }
