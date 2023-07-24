b06c9fdb1dfd9f0ce2e0e99e5c0a36bac97d926e
allykkk <ally.appskk@gmail.com>
2023-07-24 18:32:02 +0300

    deleted not relevant files

--- Week11/Day5/memory-game/src/App.js
+++ Week11/Day5/memory-game/src/App.js
@@ -4,20 +4,43 @@ import Header from './components/header';
 import Jumbotron from './components/jumbotron';
 import HeroCard from './components/heroCard';
 import data from "../src/components/data/superheroes.json";
-
+import _ from 'underscore'
 
 class App extends Component {
-  state = { score: 0, topScore: 0, heroes: data.superheroes };
+
+  state = { heroes: data.superheroes, topScore: 0, game: [] };
+
+  clickHandler = (hero) => {
+    let game = [...this.state.game]
+    if (this.duplicateHandler(game, hero)) return
+    game.push(hero.id)
+    let heroes = { ...this.state.heroes }
+    heroes = _.shuffle(heroes)
+    this.setState({ heroes, game })
+  }
+
+  duplicateHandler = (array, currentItem) => {
+    const isDuplicate = array.some(item => item === currentItem.id)
+    if (isDuplicate) {
+      const currentScore = array.length
+      if (currentScore > this.state.topScore)
+        this.setState({ topScore: currentScore })
+      this.setState({ game: [] })
+      return true
+    }
+    return false
+  }
+
 
   render() {
     return (<div className='wrapper'>
       <header className='container-fluid fixed-top'>
-        <Header score={this.state.score} topScore={this.state.topScore} />
+        <Header score={this.state.game.length} topScore={this.state.topScore} />
       </header>
       <div className='jumbotron jumbotron-fluid'>
         <Jumbotron />
       </div>
-      <HeroCard heroes={this.state.heroes} />
+      <HeroCard heroes={this.state.heroes} clickHandler={this.clickHandler} />
     </div>);
   }
 }
--- Week11/Day5/memory-game/src/components/data/data.js
+++
@@ -1,4 +0,0 @@
-// import data from './superheroes.json'
-let data = require('./superheroes.json')
-// console.log(data.superheroes)
-data.superheroes.map(hero=>console.log("Hi",hero))
